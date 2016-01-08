ab141e8caa4c5d9c5a12881a668ae8273b85b0e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-08 01:16:59 +0100

    Load script in all public pages

--- wp-web-push-main.php
+++ wp-web-push-main.php
@@ -4,12 +4,17 @@ class WebPush_Main {
   private static $instance;
 
   public function __construct() {
+    add_action('wp_enqueue_scripts', array($this, 'enqueue_frontend_scripts'));
   }
 
   public static function init() {
     if (!self::$instance) {
       self::$instance = new self();
     }
+  }
+
+  public function enqueue_frontend_scripts() {
+    wp_enqueue_script('sw-registration-script', plugins_url('lib/js/sw-manager.js', __FILE__ ));
   }
 }
 
