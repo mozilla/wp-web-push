82d6da413ed883d4b8456a7712dc6282d4bf6caf
David Walsh <davidwalsh83@gmail.com>
2016-01-11 15:13:11 -0600

    First step in localization

--- wp-web-push/wp-web-push-admin.php
+++ wp-web-push/wp-web-push-admin.php
@@ -1,5 +1,7 @@
 <?php
 
+load_plugin_textdomain( 'wpwebpush', '', dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+
 class WebPush_Admin {
   private static $instance;
 
@@ -14,7 +16,7 @@ class WebPush_Admin {
   }
 
   public function on_admin_menu() {
-    add_options_page('Web Push Options', 'Web Push', 'manage_options', 'web-push-options', array($this, 'options'));
+    add_options_page(__('Web Push Options', 'wpwebpush'), __('Web Push', 'wpwebpush'), 'manage_options', 'web-push-options', array($this, 'options'));
   }
 
   public function options() {
@@ -26,34 +28,34 @@ class WebPush_Admin {
       } else if ($_POST['webpush_title'] === 'custom') {
         update_option('webpush_title', $_POST['webpush_title_custom']);
       } else {
-        wp_die('Wrong value for the Notification Title');
+        wp_die(__('Wrong value for the Notification Title', 'wpwebpush'));
       }
 ?>
-<div class="updated"><p><strong>Settings saved.</strong></p></div>
+<div class="updated"><p><strong><?php _e('Settings saved'); ?></strong></p></div>
 <?php
     }
 ?>
 
 <div class="wrap">
-<h2>Web Push</h2>
+<h2><?php _e('Web Push', 'wpwebpush'); ?></h2>
 
 <form method="post" action="">
 <table class="form-table">
 
 <tr>
-<th scope="row">Notification Title</th>
+<th scope="row"><?php _e('Notification Title', 'wpwebpush'); ?></th>
 <td>
 <fieldset>
-<label><input type="radio" name="webpush_title" value="blog_title" <?php echo $title_option === 'blog_title' ? 'checked' : '' ?> /> Use the Site Title: <b><?php echo get_bloginfo('name'); ?></b></label><br />
-<label><input type="radio" name="webpush_title" value="custom" <?php echo $title_option !== 'blog_title' ? 'checked' : '' ?> /> Custom:</label>
-<input type="text" name="webpush_title_custom" value="<?php echo $title_option !== 'blog_title' ? $title_option : 'Write here the title' ?>" class="long-text" />
+<label><input type="radio" name="webpush_title" value="blog_title" <?php echo $title_option === 'blog_title' ? 'checked' : '' ?> /> <?php _e('Use the Site Title', 'wpwebpush'); ?>: <b><?php echo get_bloginfo('name'); ?></b></label><br />
+<label><input type="radio" name="webpush_title" value="custom" <?php echo $title_option !== 'blog_title' ? 'checked' : '' ?> /> <?php _e('Custom', 'wpwebpush'); ?>:</label>
+<input type="text" name="webpush_title_custom" value="<?php echo $title_option !== 'blog_title' ? $title_option : __('Your custom title', 'wpwebpush') ?>" class="long-text" />
 </fieldset>
 </td>
 </tr>
 </table>
 
 <p class="submit">
-<input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes" />
+<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save Changes', 'wpwebpush'); ?>" />
 </p>
 
 </form>
