dc1abcb2dd1a831afdd4089d76cb089bd9aacb6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-11 17:51:32 +0100

    Focus an already open page of the blog or open a new one when a notification is clicked. Fixes #17

--- lib/js/sw.php
+++ lib/js/sw.php
@@ -6,6 +6,19 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(self.clients.claim());
 });
 
+self.addEventListener('notificationclick', function(event) {
+  event.waitUntil(
+    self.clients.matchAll()
+    .then(function(clientList) {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+
+      return self.clients.openWindow('<?php bloginfo('url'); ?>');
+    })
+  );
+});
+
 self.addEventListener('push', function(event) {
   event.waitUntil(
     fetch('<?php echo admin_url('admin-ajax.php'); ?>?action=webpush_get_payload')
