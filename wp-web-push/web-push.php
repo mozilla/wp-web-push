5c9395129782b37bca27c5a1fb5abcc8f694192e
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-14 18:14:50 +0100

    Support sending notifications to GCM endpoints. Fixes #13

--- wp-web-push/web-push.php
+++ wp-web-push/web-push.php
@@ -1,15 +1,35 @@
 <?php
 
-function sendNotification($endpoint) {
-  $result = wp_remote_post($endpoint, array(
+define('GCM_REQUEST_URL', 'https://android.googleapis.com/gcm/send');
+
+function sendNotification($endpoint, $gcmKey) {
+  $headers = array();
+  $expectedResponseCode = 201;
+  $requestURL = $endpoint;
+  $body = '';
+
+  if (strpos($endpoint, GCM_REQUEST_URL) === 0) {
+    $endpointSections = explode('/', $endpoint);
+    $subscriptionId = $endpointSections[sizeof($endpointSections) - 1];
+    $body = json_encode(array('registration_ids' => array($subscriptionId)));
+
+    $headers['Authorization'] = 'key=' . $gcmKey;
+    $headers['Content-Type'] = 'application/json';
+    $headers['Content-Length'] = strlen($body);
+    $requestURL = GCM_REQUEST_URL;
+    $expectedResponseCode = 200;
+  } else {
+    // Ask the push service to store the message for 4 weeks.
+    $headers['TTL'] = 2419200;
+  }
+
+  $result = wp_remote_post($requestURL, array(
     'blocking' => true,
-    'headers' => array(
-      // Ask the push service to store the message for 4 weeks.
-      'TTL' => 2419200,
-    ),
+    'headers' => $headers,
+    'body' => $body,
   ));
 
-  return $result['response']['code'] === 201;
+  return $result['response']['code'] === $expectedResponseCode;
 }
 
 ?>
