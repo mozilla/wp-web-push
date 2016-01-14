b383e1c9519ec6d2145494000b08cc4af541f1d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-14 16:27:38 +0100

    Don't load and don't initialize the WebPush_Admin class if we're not in the admin section

--- wp-web-push/wp-web-push.php
+++ wp-web-push/wp-web-push.php
@@ -6,10 +6,13 @@ Text Domain: wpwebpush
 
 require_once(plugin_dir_path(__FILE__) . 'wp-web-push-main.php');
 require_once(plugin_dir_path(__FILE__) . 'wp-web-push-db.php');
-require_once(plugin_dir_path(__FILE__) . 'wp-web-push-admin.php');
 
 WebPush_Main::init();
-WebPush_Admin::init();
+
+if (is_admin()) {
+  require_once(plugin_dir_path(__FILE__) . 'wp-web-push-admin.php');
+  WebPush_Admin::init();
+}
 
 register_activation_hook(__FILE__, array('WebPush_DB', 'on_activate'));
 register_deactivation_hook(__FILE__, array('WebPush_DB', 'on_deactivate'));
