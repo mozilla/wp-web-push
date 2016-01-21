58ecc23d8c81aa36160824f756b9b079ed96609d
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-21 16:33:19 +0000

    Show a notice if the GCM info hasn't been entered yet. Fixes #45

--- wp-web-push/wp-web-push-admin.php
+++ wp-web-push/wp-web-push-admin.php
@@ -8,6 +8,15 @@ class WebPush_Admin {
   public function __construct() {
     add_action('admin_menu', array($this, 'on_admin_menu'));
     add_action('wp_dashboard_setup', array($this, 'add_dashboard_widgets'));
+    add_action('admin_notices', array($this, 'on_admin_notices'));
+  }
+
+  function on_admin_notices() {
+    $options_url = add_query_arg(array('page' => 'web-push-options'), admin_url('options-general.php'));
+
+    if (!get_option('webpush_gcm_key') or !get_option('webpush_gcm_sender_id')) {
+      echo '<div class="error"><p>' . sprintf(__('You need to set up the GCM-specific information in order to make push notifications work on Google Chrome. <a href="%s">Do it now</a>.', 'wpwebpush'), $options_url) . '</p></div>';
+    }
   }
 
   function add_dashboard_widgets() {
