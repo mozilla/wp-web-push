9fdd3ab564b53d5a541693e8d7ab0dfa6d668804
Salvador de la Puente González <salva@unoyunodiez.com>
2016-02-11 18:22:29 +0100

    Wrapping SW functionality inside an IIFE

--- wp-web-push/lib/js/sw.php
+++ wp-web-push/lib/js/sw.php
@@ -1,78 +1,80 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(self.skipWaiting());
-});
+(function(self) {
+  self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting());
+  });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim());
-});
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+  });
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
+  self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
 
-  var data = event.notification.data;
-  if (!data) {
-    return;
-  }
+    var data = event.notification.data;
+    if (!data) {
+      return;
+    }
 
-  var url = data.url;
+    var url = data.url;
 
-  event.waitUntil(
-    self.clients.matchAll({
-      type: "window",
-    })
-    .then(function(clientList) {
-      for (var i = 0; i < clientList.length; i++) {
-        if (clientList[i].url === url) {
-          return clientList[i].focus();
+    event.waitUntil(
+      self.clients.matchAll({
+        type: "window",
+      })
+      .then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          if (clientList[i].url === url) {
+            return clientList[i].focus();
+          }
         }
-      }
 
-      var newURL = new URL(url);
-      if (newURL.search) {
-        newURL.search += '&';
-      } else {
-        newURL.search += '?';
-      }
-      newURL.search += 'webpush_post_id=' + data.postID;
+        var newURL = new URL(url);
+        if (newURL.search) {
+          newURL.search += '&';
+        } else {
+          newURL.search += '?';
+        }
+        newURL.search += 'webpush_post_id=' + data.postID;
 
-      return self.clients.openWindow(newURL);
-    })
-  );
-});
+        return self.clients.openWindow(newURL);
+      })
+    );
+  });
 
-self.addEventListener('push', function(event) {
-  event.waitUntil(
-    fetch('<?php echo admin_url('admin-ajax.php'); ?>?action=webpush_get_payload')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      return self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: data.icon,
-        data: data,
-      });
-    })
-  );
-});
+  self.addEventListener('push', function(event) {
+    event.waitUntil(
+      fetch('<?php echo admin_url('admin-ajax.php'); ?>?action=webpush_get_payload')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        return self.registration.showNotification(data.title, {
+          body: data.body,
+          icon: data.icon,
+          data: data,
+        });
+      })
+    );
+  });
 
-self.addEventListener('pushsubscriptionchange', function(event) {
-  event.waitUntil(
-    self.registration.pushManager.subscribe({
-      userVisibleOnly: true,
-    })
-    .then(function(subscription) {
-      var key = subscription.getKey ? subscription.getKey('p256dh') : '';
+  self.addEventListener('pushsubscriptionchange', function(event) {
+    event.waitUntil(
+      self.registration.pushManager.subscribe({
+        userVisibleOnly: true,
+      })
+      .then(function(subscription) {
+        var key = subscription.getKey ? subscription.getKey('p256dh') : '';
 
-      var formData = new FormData();
-      formData.append('action', 'webpush_register');
-      formData.append('endpoint', subscription.endpoint);
-      formData.append('key', key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '');
+        var formData = new FormData();
+        formData.append('action', 'webpush_register');
+        formData.append('endpoint', subscription.endpoint);
+        formData.append('key', key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '');
 
-      return fetch('<?php echo admin_url('admin-ajax.php'); ?>', {
-        method: 'post',
-        body: formData,
-      });
-    })
-  );
-});
+        return fetch('<?php echo admin_url('admin-ajax.php'); ?>', {
+          method: 'post',
+          body: formData,
+        });
+      })
+    );
+  });
+}(self));
