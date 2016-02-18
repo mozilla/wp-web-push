df053b880e3e674148a1a4c8008c97f73eb9db7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-02-18 15:45:07 +0000

    Rename subscription button to subscription icon in the admin settings

--- wp-web-push/wp-web-push-admin.php
+++ wp-web-push/wp-web-push-admin.php
@@ -258,7 +258,7 @@ class WebPush_Admin {
 <tr>
 <th scope="row"></th>
 <td>
-<label><input type="checkbox" name="webpush_subscription_button" <?php echo $subscription_button_option ? 'checked' : ''; ?> /> <?php _e('Show subscription button', 'web-push'); ?></label>
+<label><input type="checkbox" name="webpush_subscription_button" <?php echo $subscription_button_option ? 'checked' : ''; ?> /> <?php _e('Show subscription icon', 'web-push'); ?></label>
 <p class="description"><?php _e('A button in the bottom-right corner of the page that the user can use to subscribe/unsubscribe.')?></p>
 </td>
 </tr>
