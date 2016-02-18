709aaed7034feffb7af1f48cb00ae5198f660b57
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-02-18 15:26:00 +0000

    Obey option chosen in the meta box

--- wp-web-push/wp-web-push-main.php
+++ wp-web-push/wp-web-push-main.php
@@ -143,8 +143,7 @@ class WebPush_Main {
       return;
     }
 
-    if (!in_array('update-post', get_option('webpush_triggers')) and
-        ($old_status === 'publish' or !in_array('new-post', get_option('webpush_triggers')))) {
+    if (!isset($_REQUEST['webpush_send_notification'])) {
       return;
     }
 
