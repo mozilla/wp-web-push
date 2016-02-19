cfaf0291e8ddc6e3b4a4536ffea51f2754efd16e
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-02-19 15:15:57 +0000

    Remove now unneeded inline preview for the title

--- wp-web-push/wp-web-push-admin.php
+++ wp-web-push/wp-web-push-admin.php
@@ -234,7 +234,7 @@ class WebPush_Admin {
 <th scope="row"><?php _e('Title', 'web-push'); ?></th>
 <td>
 <fieldset>
-<label><input type="radio" name="webpush_title" value="blog_title" <?php echo $title_option === 'blog_title' ? 'checked' : ''; ?> /> <?php _e('Use the Site Title', 'web-push'); ?>: <b><?php echo get_bloginfo('name'); ?></b></label><br />
+<label><input type="radio" name="webpush_title" value="blog_title" <?php echo $title_option === 'blog_title' ? 'checked' : ''; ?> /> <?php _e('Use the Site Title', 'web-push'); ?></label><br />
 <label><input type="radio" name="webpush_title" value="custom" <?php echo $title_option !== 'blog_title' ? 'checked' : ''; ?> /> <?php _e('Custom:'); ?></label>
 <input type="text" id="webpush_title_custom" name="webpush_title_custom" value="<?php echo $title_option !== 'blog_title' ? $title_option : esc_attr__('Your custom title', 'web-push'); ?>" class="long-text" />
 </fieldset>
