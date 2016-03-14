305c95405c697b4133ba3e1fdabdd45fc90d69ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-03-14 22:27:28 +0000

    Rewrite manifest tests

--- tests/test-manifest.php
+++ tests/test-manifest.php
@@ -3,94 +3,36 @@
 // TODO: Some of these tests should be moved to the WebAppManifestGenerator repository (https://github.com/marco-c/wp-web-app-manifest-generator).
 
 class ManifestTest extends WP_Ajax_UnitTestCase {
+  function setUp() {
+    parent::setUp();
+    remove_all_actions('wp_head');
+  }
+
   function test_generate_manifest_link() {
-    $this->assertEquals('<link rel="manifest" href="/?webappmanifest_file=manifest">', get_echo(array(WebAppManifestGenerator::getInstance(), 'add_manifest')));
+    $this->assertEquals('<link rel="manifest" href="http://example.org/wp-content/uploads/wpservefile_files/manifest.json">', get_echo(array(WebAppManifestGenerator::getInstance(), 'add_manifest')));
   }
 
-  function test_manifest_request_no_gcm_sender_id() {
+  function test_WebPush_Main_doesnt_initialize_WebAppManifestGenerator_if_no_gcm_sender_id() {
     $main = new WebPush_Main();
-
-    $query = (object)array(
-      'query_vars' => array(
-        'webappmanifest_file' => 'manifest',
-      ),
-    );
-
-    $result = '';
-    try {
-      $result += get_echo(array(WebAppManifestGenerator::getInstance(), 'on_parse_request'), array($query));
-      $this->assertTrue(false);
-    } catch (WPAjaxDieContinueException $e) {
-      $this->assertTrue(true);
-    }
-
-    $this->assertEquals('{"start_url":"\/"}', $this->_last_response);
+    $this->assertFalse(has_action('wp_head'));
   }
 
-  function test_manifest_request() {
-    update_option('webpush_gcm_sender_id', '42');
+  function test_WebAppManifestGenerator_registers_wp_head() {
+    $main = new WebAppManifestGenerator();
+    $this->assertTrue(has_action('wp_head'));
+  }
 
-    $main = new WebPush_Main();
+  function test_manifest_generation() {
+    WebAppManifestGenerator::getInstance()->set_field('test', 'Marco');
 
-    $query = (object)array(
-      'query_vars' => array(
-        'webappmanifest_file' => 'manifest',
-      ),
-    );
+    $result = WebAppManifestGenerator::getInstance()->manifestJSONGenerator();
 
-    $result = '';
-    try {
-      $result += get_echo(array(WebAppManifestGenerator::getInstance(), 'on_parse_request'), array($query));
-      $this->assertTrue(false);
-    } catch (WPAjaxDieContinueException $e) {
-      $this->assertTrue(true);
-    }
-
-    $this->assertEquals('{"start_url":"\/","gcm_sender_id":"42","gcm_user_visible_only":true}', $this->_last_response);
+    $this->assertEquals('{"start_url":"\/","test":"Marco"}', $result['content']);
+    $this->assertEquals('application/json', $result['contentType']);
   }
 
   function test_manifest_request_multiple_users() {
-    update_option('webpush_gcm_sender_id', '42');
-
     require dirname(dirname(__FILE__)) . '/build/vendor/marco-c/wp-web-app-manifest-generator/WebAppManifestGenerator.php';
-
-    WebAppManifestGenerator::getInstance()->set_field('test', 'Marco');
-
-    $main = new WebPush_Main();
-
-    $query = (object)array(
-      'query_vars' => array(
-        'webappmanifest_file' => 'manifest',
-      ),
-    );
-
-    $result = '';
-    try {
-      $result += get_echo(array(WebAppManifestGenerator::getInstance(), 'on_parse_request'), array($query));
-      $this->assertTrue(false);
-    } catch (WPAjaxDieContinueException $e) {
-      $this->assertTrue(true);
-    }
-
-    $this->assertEquals('{"start_url":"\/","gcm_sender_id":"42","gcm_user_visible_only":true,"test":"Marco"}', $this->_last_response);
-  }
-
-  function test_dont_echo_manifest_if_query_doesnt_contain_required_param() {
-    $query = (object)array(
-      'query_vars' => array(),
-    );
-
-    WebAppManifestGenerator::getInstance()->on_parse_request($query);
-  }
-
-  function test_query_vars() {
-    $this->assertContains('webappmanifest_file', WebAppManifestGenerator::getInstance()->on_query_vars(array()));
-  }
-
-  function test_dont_overwrite_query_vars() {
-    $query_vars = WebAppManifestGenerator::getInstance()->on_query_vars(array('an_already_existing_query_var'));
-    $this->assertContains('webappmanifest_file', $query_vars);
-    $this->assertContains('an_already_existing_query_var', $query_vars);
   }
 }
 
