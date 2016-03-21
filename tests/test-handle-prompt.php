a476e5dacfaba57a67685fcc5c7c9c3bdd53022c
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-03-21 16:19:53 +0100

    Test non-'nopriv' alternative of the AJAX calls

--- tests/test-handle-prompt.php
+++ tests/test-handle-prompt.php
@@ -11,6 +11,17 @@ class HandlePromptTest extends WP_Ajax_UnitTestCase {
 
     $this->assertEquals(1, get_option('webpush_prompt_count'));
   }
+
+  function test_prompt_priv() {
+    try {
+      $this->_handleAjax('webpush_prompt');
+      $this->assertTrue(false);
+    } catch (WPAjaxDieStopException $e) {
+      $this->assertTrue(true);
+    }
+
+    $this->assertEquals(1, get_option('webpush_prompt_count'));
+  }
 }
 
 ?>
