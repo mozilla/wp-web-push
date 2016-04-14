361e3f22c1822eb7f95ba4c8e443a5d694e2a656
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-04-14 15:13:58 +0100

    Remove pre_http_request filters manually in the send notification test

--- tests/test-send-notification.php
+++ tests/test-send-notification.php
@@ -3,6 +3,11 @@
 require_once dirname(dirname(__FILE__)) . '/build/web-push.php';
 
 class SendNotificationTest extends WP_UnitTestCase {
+  function tearDown() {
+    parent::tearDown();
+    remove_all_filters('pre_http_request');
+  }
+
   function send_webpush_notification_success($forceWP) {
     $oldNum = getSentNotificationNum();
 
