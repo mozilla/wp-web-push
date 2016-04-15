62ebca18d3192805a0d3dc09521d1daf9fcb170a
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-04-15 15:47:20 +0100

    Relax PHP dependency to 5.4

--- wp-web-push/wp-web-push.php
+++ wp-web-push/wp-web-push.php
@@ -10,7 +10,7 @@ License: GPLv2 or later
 Text Domain: web-push
 */
 
-define('USE_VAPID', version_compare(phpversion(), '5.5') >= 0 && // PHP 5.5+
+define('USE_VAPID', version_compare(phpversion(), '5.4') >= 0 && // PHP 5.4+
                     function_exists('mcrypt_encrypt') &&         // ext-mcrypt (https://packagist.org/packages/mdanter/ecc)
                     function_exists('gmp_mod') &&                // ext-gmp (https://packagist.org/packages/mdanter/ecc)
                     function_exists('openssl_encrypt')           // ext-openssl (https://packagist.org/packages/lcobucci/jwt)
