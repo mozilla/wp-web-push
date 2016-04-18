6aa9afde841d08167d14f88cd001c0d468f8667a
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-04-18 17:03:33 +0100

    Use the 'regular-text' class for the title field

--- wp-web-push/wp-web-push-admin.php
+++ wp-web-push/wp-web-push-admin.php
@@ -293,7 +293,7 @@ class WebPush_Admin {
 <fieldset>
 <label><input type="radio" name="webpush_title" value="blog_title" <?php echo $title_option === 'blog_title' ? 'checked' : ''; ?> /> <?php _e('Use the Site Title', 'web-push'); ?></label><br />
 <label><input type="radio" name="webpush_title" value="custom" <?php echo $title_option !== 'blog_title' ? 'checked' : ''; ?> /> <?php _e('Custom:'); ?></label>
-<input type="text" id="webpush_title_custom" name="webpush_title_custom" value="<?php echo $title_option !== 'blog_title' ? $title_option : esc_attr__('Your custom title', 'web-push'); ?>" class="long-text" />
+<input type="text" id="webpush_title_custom" name="webpush_title_custom" value="<?php echo $title_option !== 'blog_title' ? $title_option : esc_attr__('Your custom title', 'web-push'); ?>" class="regular-text" />
 </fieldset>
 </td>
 </tr>
